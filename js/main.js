31c3d1b95f2353e57c1e2720dcbff0e4d88b1be4
Tiep Phan <tieppt12@gmail.com>
2017-05-22 20:34:30 +0700

    fix coding style

--- js/main.js
+++ js/main.js
@@ -29,22 +29,13 @@ $(document).ready(function(){
 
 
 	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+		});
+	}
 
 });
 
